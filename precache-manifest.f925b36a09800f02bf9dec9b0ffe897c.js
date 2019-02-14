95b8454fcaf24923997ba6f033058e7fa1c2d548
Hugo Anderson <hugochiyubianderson@gmail.com>
2019-02-13 23:43:30 -0800

    Updates

---
+++ precache-manifest.f925b36a09800f02bf9dec9b0ffe897c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b8d45845eae84290e9b",
+    "url": "/bodyworks/static/css/main.9fa859fa.chunk.css"
+  },
+  {
+    "revision": "9b8d45845eae84290e9b",
+    "url": "/bodyworks/static/js/main.9b8d4584.chunk.js"
+  },
+  {
+    "revision": "20009964e7c448daeaeb",
+    "url": "/bodyworks/static/js/runtime~main.20009964.js"
+  },
+  {
+    "revision": "7b507d6c1e75bf420684",
+    "url": "/bodyworks/static/js/2.7b507d6c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bodyworks/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "49c62f74eff16d082867f10fa9343962",
+    "url": "/bodyworks/index.html"
+  }
+];
